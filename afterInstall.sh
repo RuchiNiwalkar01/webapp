c79aae33e3733a3862c6d1853746da6cf1789b50
Ruchi Niwalkar <niwalkar.r@husky.neu.edu>
2020-03-11 16:24:05 -0400

    testing v1/bills url 6

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu
 sudo chown -R ubuntu:ubuntu /home/ubuntu/
+source /etc/profile
 sudo chmod +x /home/ubuntu/webapp-0.0.1-SNAPSHOT.jar
 kill -9 $(ps -ef | grep webapp | grep -v grep | awk '{print $2}')
 nohup java -jar /home/ubuntu/webapp-0.0.1-SNAPSHOT.jar > /home/ubuntu/output.txt &
