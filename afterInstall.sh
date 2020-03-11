64e83a881eae216760a8869a5046468f835a9e7c
Ruchi Niwalkar <niwalkar.r@husky.neu.edu>
2020-03-10 22:39:57 -0400

    appSpec.yml and changes in configyml 1

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu
-sudo chown -R root:root /home/ubuntu/
+sudo chown -R ubuntu:ubuntu /home/ubuntu/
 source /etc/profile
 sudo chmod +x /home/ubuntu/webapp-0.0.1-SNAPSHOT.jar
-sudo kill -9 $(lsof -t -i:8080)
+sudo kill -9 $(sudo lsof -i tcp:8080)
 nohup java -jar /home/ubuntu/webapp-0.0.1-SNAPSHOT.jar > /home/ubuntu/output.txt &
