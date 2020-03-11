42f08b46405762ec37cb3a2da5149072df64dbde
Ruchi Niwalkar <niwalkar.r@husky.neu.edu>
2020-03-11 01:03:55 -0400

    Testing v2/bills end point 4

--- afterInstall.sh
+++ afterInstall.sh
@@ -3,5 +3,5 @@ cd /home/ubuntu
 sudo chown -R ubuntu:ubuntu /home/ubuntu/
 source /etc/profile
 sudo chmod +x /home/ubuntu/webapp-0.0.1-SNAPSHOT.jar
-sudo fuser -k 8080/tcp
+sudo kill -9 `sudo lsof -t -i:8080`
 nohup java -jar /home/ubuntu/webapp-0.0.1-SNAPSHOT.jar > /home/ubuntu/output.txt &
