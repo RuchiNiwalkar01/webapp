1b44f2c24fdfea071c7123993585d9b444ef12e7
Ruchi Niwalkar <niwalkar.r@husky.neu.edu>
2020-03-11 14:41:33 -0400

    testing v1/bills url 3

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 cd /home/ubuntu
 sudo chown -R ubuntu:ubuntu /home/ubuntu/
-pid=$(ps aux | grep "java -jar" | awk '{print $2}')
+pid=$(ps aux | grep "java -jar" | grep "webapp-0.0.1-SNAPSHOT.jar" | awk '{print $2}')
 sudo kill -9 "$pid" 
 source /etc/profile
 sudo chmod +x /home/ubuntu/webapp-0.0.1-SNAPSHOT.jar
-#sudo kill -9 $(sudo lsof -i tcp:8080)
 nohup java -jar /home/ubuntu/webapp-0.0.1-SNAPSHOT.jar > /home/ubuntu/output.txt &
